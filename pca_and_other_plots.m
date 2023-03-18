a8cee13b8bfa5d2711b7c1a94619253cb63818ec
Shankho Niyogi <31923000+Shankho-Niyogi@users.noreply.github.com>
2023-03-17 17:53:59 -0700

    Update and rename pcaandothers.m to pca_and_other_plots.m

--- pca_and_other_plots.m
+++ pca_and_other_plots.m
@@ -1,7 +1,8 @@
-clear all
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-data=load('deletelater1234.txt');
+clc
+clear
+close all
+
+data=load('sample_data.txt');
 categories={'Na','Mg','Si','K','Ca','Al','Mn','Cu','Rb','Sr','Mo','Sb','Ba','U','Be','B','Zn','Pb','Li'};
 
 set(figure,'Units','normalized','Position',[0,0,1,1]);
@@ -10,7 +11,7 @@ corrmatrix = flipud(corrmatrix);
 imagesc(corrmatrix),colormap(parula);
 atick=1:1:length(categories);
 title('Correlation Matrix');
-axis square, colorbar, hold
+axis square, colorbar  hold
 set(gca,'XTick',atick);
 set(gca,'YTick',atick);
 set(gca,'XTickLabel',categories,'YTickLabel',flipud(categories))
@@ -18,19 +19,17 @@ set(gca,'XTickLabel',categories,'YTickLabel',flipud(categories))
 set(figure,'Units','normalized','Position',[0,0,1,1]);
 imagesc(corrmatrix);colormap(parula);
 caxis([-1,1])
+
+%for creating a custom colormap to group all correlations above and below a
+%threshold to be of a single color
 map=colormap;
-for i=1:length(map)
-    if i<=6
-        map(i,:) = [0 0 0];
-    else if i>(length(map)-6)
-            map(i,:)=[1 1 1];
-        end
-    end
-end
+map(1:6, :) = repmat([0 0 0], 6, 1);
+map(end-5:end, :) = repmat([1 1 1], 6, 1);
+
 colormap(map)
 atick=1:1:length(categories);
 title('Grouped Correlation matrix');
-axis square, colorbar, hold
+axis square,  colorbar  hold
 set(gca,'XTick',atick);
 set(gca,'YTick',atick);
 set(gca,'XTickLabel',categories,'YTickLabel',flipud(categories))
@@ -38,7 +37,7 @@ set(gca,'XTickLabel',categories,'YTickLabel',flipud(categories))
 figure()
 boxplot(data,'orientation','horizontal','labels',categories);
 title('Boxplot for all elements and nutrients');
-xlabel('Concentration in µg/L');
+xlabel('Concentration in Âµg/L');
 
 w=1./var(data);
 [wcoeff,score,latent,tsquared,explained]=pca(data,'VariableWeights',w);
@@ -53,8 +52,7 @@ clear coefforth
 coefforth = diag(sqrt(w))*wcoeff;
 cscores = zscore(data)*coefforth;
 [st2,index] = sort(tsquared,'descend'); % sort in descending order
-% samples=samples';
-% samples(extreme,:)
+
 
 figure()
 biplot(coefforth(:,1:2),'scores',score(:,1:2),'varlabels',categories);
@@ -64,7 +62,7 @@ figure()
 biplot(coefforth(:,1:3),'scores',score(:,1:3),'varlabels',categories);
 title('All PC (rep. as points) and score (rep. as vectors) distribution');
 
-LargeDataSet=load('deletelater1234.txt');
+LargeDataSet=load('sample_data.txt');
 LargeDataSet=LargeDataSet';
 labels ={'Na','Mg','Si','K','Ca','Al','Mn','Cu','Rb','Sr','Mo','Sb','Ba','U','Be','B','Zn','Pb','Li'};
 tree = linkage(LargeDataSet,'average'); 
@@ -78,14 +76,3 @@ hAxis = get(H(1),'parent');
 perm = str2num(get(hAxis,'XtickLabel'));
 set(hAxis,'XTickLabel',labels(perm));
 set(gca,'yscale','log');
-
-% h=findobj('type','figure'); % find the handles of the opened figures
-% folder='D:'; % Desination folder
-% for k=1:numel(h)
-%   filename=sprintf('Ganga after Confluence%d.jpg',k);
-%   file=fullfile(folder,filename);
-%   saveas(h(k),file);
-% end
-
-
-
